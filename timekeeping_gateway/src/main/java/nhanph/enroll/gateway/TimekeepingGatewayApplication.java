c451f7bc3ee2e4cc2d33f02dd8bf91a90bf8d529
nhanph <nhanphh2003@gmail.com>
2025-05-07 12:51:25 +0700

    add timekeeping gateway

---
+++ timekeeping_gateway/src/main/java/nhanph/enroll/gateway/TimekeepingGatewayApplication.java
@@ -0,0 +1,13 @@
+package nhanph.enroll.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimekeepingGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimekeepingGatewayApplication.class, args);
+    }
+
+}
