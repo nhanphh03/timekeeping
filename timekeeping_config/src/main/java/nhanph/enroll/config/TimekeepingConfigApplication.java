22ec88276e2bcd076069e8f0e41e3e03b2985834
nhanph <nhanphh2003@gmail.com>
2025-05-07 12:54:15 +0700

    add timekeeping config

---
+++ timekeeping_config/src/main/java/nhanph/enroll/config/TimekeepingConfigApplication.java
@@ -0,0 +1,16 @@
+package nhanph.enroll.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author nhanph
+ */
+@SpringBootApplication
+public class TimekeepingConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimekeepingConfigApplication.class, args);
+    }
+
+}
