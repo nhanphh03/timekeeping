3804e8722f2ef9bb17abe3f9f1f4d952a2dba3aa
Pham Huu Nhan <102335308+nhanphh03@users.noreply.github.com>
2025-05-07 10:06:32 +0000

    update codespace

--- timekeeping_producer/src/main/java/nhanph/timekeeping/producer/controller/KafkaController.java
+++ timekeeping_producer/src/main/java/nhanph/timekeeping/producer/controller/KafkaController.java
@@ -1,31 +1,30 @@
-package nhanph.timekeeping.producer.controller;
+// package nhanph.timekeeping.producer.controller;
 
-import nhanph.timekeeping.producer.service.KafkaProducerService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+// import nhanph.timekeeping.producer.service.KafkaProducerService;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.RestController;
 
-/**
- * {@code @Package:} nhanph.timekeeping.producer.controller
- * {@code @author:} nhanph
- * {@code @date:} 05/07/2025
- * {@code @Copyright:} @nhanph
- */
+// /**
+//  * {@code @Package:} nhanph.timekeeping.producer.controller
+//  * {@code @author:} nhanph
+//  * {@code @date:} 05/07/2025
+//  * {@code @Copyright:} @nhanph
+//  */
 
-@RestController
-public class KafkaController {
+// @RestController
+// public class KafkaController {
 
-    private final KafkaProducerService producerService;
+//     private final KafkaProducerService producerService;
 
-    @Autowired
-    public KafkaController(KafkaProducerService producerService) {
-        this.producerService = producerService;
-    }
+//     public KafkaController(KafkaProducerService producerService) {
+//         this.producerService = producerService;
+//     }
 
-    @PostMapping("/sendMessage")
-    public String sendMessage(@RequestBody String message) {
-        producerService.sendMessage(message, "timekeeping-topic");
-        return "Message sent to Kafka: " + message;
-    }
-}
+//     @PostMapping("/sendMessage")
+//     public String sendMessage(@RequestBody String message) {
+//         producerService.sendMessage(message, "timekeeping-topic");
+//         return "Message sent to Kafka: " + message;
+//     }
+// }
--- timekeeping_producer/src/main/java/nhanph/timekeeping/producer/service/KafkaProducerService.java
+++ timekeeping_producer/src/main/java/nhanph/timekeeping/producer/service/KafkaProducerService.java
@@ -1,29 +1,29 @@
-package nhanph.timekeeping.producer.service;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+// package nhanph.timekeeping.producer.service;
+// import org.springframework.kafka.core.KafkaTemplate;
+// import org.springframework.kafka.annotation.EnableKafka;
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.stereotype.Component;
 
-/**
- * {@code @Package:} nhanph.timekeeping.producer.service
- * {@code @author:} nhanph
- * {@code @date:} 05/07/2025
- * {@code @Copyright:} @nhanph
- */
-@EnableKafka
-@Component
-public class KafkaProducerService {
+// /**
+//  * {@code @Package:} nhanph.timekeeping.producer.service
+//  * {@code @author:} nhanph
+//  * {@code @date:} 05/07/2025
+//  * {@code @Copyright:} @nhanph
+//  */
+// @EnableKafka
+// @Component
+// public class KafkaProducerService {
 
-    @Value("${spring.kafka.bootstrap-servers}")
-    private String bootstrapServers;
+//     @Value("${spring.kafka.bootstrap-servers}")
+//     private String bootstrapServers;
 
-    private final KafkaTemplate<String, String> kafkaTemplate;
+//     private final KafkaTemplate<String, String> kafkaTemplate;
 
-    public KafkaProducerService(KafkaTemplate<String, String> kafkaTemplate) {
-        this.kafkaTemplate = kafkaTemplate;
-    }
+//     public KafkaProducerService(KafkaTemplate<String, String> kafkaTemplate) {
+//         this.kafkaTemplate = kafkaTemplate;
+//     }
 
-    public void sendMessage(String message, String topic) {
-        kafkaTemplate.send(topic, message);
-    }
-}
+//     public void sendMessage(String message, String topic) {
+//         kafkaTemplate.send(topic, message);
+//     }
+// }
