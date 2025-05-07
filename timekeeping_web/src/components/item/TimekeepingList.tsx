c5d20c8dc7a711b00d97faa7ad894b7846faf5ec
Pham Huu Nhan <102335308+nhanphh03@users.noreply.github.com>
2025-05-07 04:07:43 +0000

    update props timekeeping list

--- timekeeping_web/src/components/item/TimekeepingList.tsx
+++ timekeeping_web/src/components/item/TimekeepingList.tsx
@@ -1,80 +1,80 @@
 import {Stack} from '@mui/material';
-import TimekeepingItem from './TimekeepingItem.js';
+import TimekeepingItem from './TimekeepingItem';
 
 export default function TimekeepingList() {
     return (
         <Stack spacing={1} sx={{p: 2}}>
             <TimekeepingItem
-                index={'02'}
+                index={'01'}
                 imageSrc="http://100.83.174.102:8181/api/v1/file/media/avatar/2003"
                 name="Nguyễn Văn A"
+                title="Nhan"
+                timeOut="08:00 - 17:00"
                 timeIn="08:00 - 17:00"
-                timeOut="08:15 - 17:05"
-                title="Lotte Finance"
+            />
+            <TimekeepingItem
+                index={'02'}
+                imageSrc="http://100.83.174.102:8181/api/v1/file/media/avatar/2003"
+                title="Nhan"
+                name="Trần Thị B"
+                timeOut="08:00 - 17:00"
+                timeIn="08:00 - 17:00"
             />
             <TimekeepingItem
                 index={'03'}
                 imageSrc="http://100.83.174.102:8181/api/v1/file/media/avatar/2003"
+                title="Nhan"
                 name="Trần Thị B"
-                timeIn="08:15 - 17:05"
-                timeOut="08:15 - 17:05"
-                title="Lotte Finance"
+                timeOut="08:00 - 17:00"
+                timeIn="08:00 - 17:00"
             />
             <TimekeepingItem
                 index={'04'}
                 imageSrc="http://100.83.174.102:8181/api/v1/file/media/avatar/2003"
+                title="Nhan"
                 name="Trần Thị B"
-                timeIn="08:15 - 17:05"
-                timeOut="08:15 - 17:05"
-                title="Lotte Finance"
+                timeOut="08:00 - 17:00"
+                timeIn="08:00 - 17:00"
             />
             <TimekeepingItem
                 index={'05'}
                 imageSrc="http://100.83.174.102:8181/api/v1/file/media/avatar/2003"
+                title="Nhan"
                 name="Trần Thị B"
-                timeIn="08:15 - 17:05"
-                timeOut="08:15 - 17:05"
-                title="Lotte Finance"
+                timeOut="08:00 - 17:00"
+                timeIn="08:00 - 17:00"
             />
             <TimekeepingItem
                 index={'06'}
                 imageSrc="http://100.83.174.102:8181/api/v1/file/media/avatar/2003"
+                title="Nhan"
                 name="Trần Thị B"
-                timeIn="08:15 - 17:05"
-                timeOut="08:15 - 17:05"
-                title="Lotte Finance"
+                timeOut="08:00 - 17:00"
+                timeIn="08:00 - 17:00"
             />
             <TimekeepingItem
                 index={'07'}
                 imageSrc="http://100.83.174.102:8181/api/v1/file/media/avatar/2003"
+                title="Nhan"
                 name="Trần Thị B"
-                timeIn="08:15 - 17:05"
-                timeOut="08:15 - 17:05"
-                title="Lotte Finance"
+                timeOut="08:00 - 17:00"
+                timeIn="08:00 - 17:00"
             />
             <TimekeepingItem
                 index={'08'}
                 imageSrc="http://100.83.174.102:8181/api/v1/file/media/avatar/2003"
+                title="Nhan"
                 name="Trần Thị B"
-                timeIn="08:15 - 17:05"
-                timeOut="08:15 - 17:05"
-                title="Lotte Finance"
+                timeOut="08:00 - 17:00"
+                timeIn="08:00 - 17:00"
             />
             <TimekeepingItem
                 index={'09'}
                 imageSrc="http://100.83.174.102:8181/api/v1/file/media/avatar/2003"
+                title="Nhan"
                 name="Trần Thị B"
-                timeIn="08:15 - 17:05"
-                timeOut="08:15 - 17:05"
-                title="Lotte Finance"
-            />
-            <TimekeepingItem
-                index={'10'}
-                imageSrc="http://100.83.174.102:8181/api/v1/file/media/avatar/2003"
-                name="Trần Thị B"
-                timeIn="08:15 - 17:05"
-                timeOut="08:15 - 17:05"
-                title="Lotte Finance"
+                timeOut="08:00 - 17:00"
+                timeIn="08:00 - 17:00"
             />
         </Stack>
     );
