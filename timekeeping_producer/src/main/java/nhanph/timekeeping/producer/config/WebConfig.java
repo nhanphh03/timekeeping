876d62be23c274fe940719251a1b0d060dab7a6c
nhanph <nhanphh2003@gmail.com>
2025-05-10 22:21:52 +0700

    add config cors and update producer service

--- timekeeping_producer/src/main/java/nhanph/timekeeping/producer/config/WebConfig.java
+++ timekeeping_producer/src/main/java/nhanph/timekeeping/producer/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Áp dụng cho tất cả các endpoint
-                .allowedOrigins("*") // Cho phép tất cả các origin
+                .allowedOrigins("http://localhost:2003", "http://192.168.1.9:2003") // Liệt kê các origin cho phép
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Cho phép các phương thức HTTP
                 .allowedHeaders("*") // Cho phép tất cả các header
                 .allowCredentials(true); // Cho phép gửi cookies nếu cần
