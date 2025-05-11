1b99159e5aab3382eab181093d3c1ddea463b74d
nhanph <nhanphh2003@gmail.com>
2025-05-11 11:36:41 +0700

    add sql script

--- timekeeping_processor_face/sql/create_table.sql
+++ timekeeping_processor_face/sql/create_table.sql
@@ -3,7 +3,7 @@ CREATE TABLE detection
     id                       BIGINT AUTO_INCREMENT PRIMARY KEY,
     image_path               VARCHAR(255),
     camera_code              VARCHAR(255),
-    people_id                VARCHAR(255),
+    customer_code                VARCHAR(255),
     recognition_status       VARCHAR(255),
     response_raw             TEXT,
     created_time             DATETIME,
@@ -13,4 +13,55 @@ CREATE TABLE detection
     search_id                VARCHAR(255),
     score DOUBLE
 );
+
+CREATE TABLE captured_images
+(
+    id                       BIGINT AUTO_INCREMENT PRIMARY KEY,
+    path_image               VARCHAR(255),
+    detected_status       VARCHAR(255),
+    captured_time            VARCHAR(255),
+    customer_code                VARCHAR(255),
+    camera_code              VARCHAR(255),
+    search_id                VARCHAR(255),
+    response_raw             TEXT,
+    response_time                VARCHAR(255)
+);
+
+create index ci_index_captured_time
+    on captured_images (captured_time);
+
+create index ci_index_customer_code_IDX
+    on captured_images (customer_code);
+
+CREATE TABLE customer
+(
+    id                       BIGINT AUTO_INCREMENT PRIMARY KEY,
+    customer_code                VARCHAR(255) null,
+    full_name                VARCHAR(255) null,
+    date_of_birth                DATETIME null,
+    gender                      VARCHAR(50) null,
+    group_id                           int null,
+    image_path                VARCHAR(255) null,
+    status               int null,
+    mobile_phone               varchar(20)  null
+);
+
+CREATE TABLE customer_type
+(
+    id                       BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name                VARCHAR(255) null,
+    created_time                DATETIME null,
+    code                      VARCHAR(50) null,
+    status               int null
+);
+
+CREATE TABLE users
+(
+    id                       BIGINT AUTO_INCREMENT PRIMARY KEY,
+    username                VARCHAR(255) null,
+    password                      VARCHAR(50) null,
+    created_time                DATETIME null,
+    status               int null
+);
+
 
