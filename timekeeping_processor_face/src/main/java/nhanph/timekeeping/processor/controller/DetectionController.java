6fab305377665661bf132fc3f8dede06deebc2f9
nhanph <nhanphh2003@gmail.com>
2025-05-11 17:31:45 +0700

    add entity

--- timekeeping_processor_face/src/main/java/nhanph/timekeeping/processor/controller/DetectionController.java
+++ timekeeping_processor_face/src/main/java/nhanph/timekeeping/processor/controller/DetectionController.java
@@ -1,10 +1,31 @@
 package nhanph.timekeeping.processor.controller;
 
+import lombok.RequiredArgsConstructor;
+import nhanph.timekeeping.processor.dto.detect.DetectionDTO;
+import nhanph.timekeeping.processor.service.DetectionService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping({"/api/detection"})
+@RequiredArgsConstructor
 public class DetectionController {
+
+    private final DetectionService detectionService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<DetectionDTO>> getAllDetection() {
+        return ResponseEntity.ok(detectionService.getAllDetection());
+    }
+
+    @GetMapping("/first")
+    public ResponseEntity<DetectionDTO> getFirstDetection() {
+        return ResponseEntity.ok(detectionService.firstDetection());
+    }
 
 }
