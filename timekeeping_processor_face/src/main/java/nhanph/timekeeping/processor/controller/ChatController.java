2f8e588358d35827cc828cd4d0fa043cb7b32cc7
Pham Huu Nhan <102335308+nhanphh03@users.noreply.github.com>
2025-05-13 11:21:30 +0700

    Update ChatController.java

--- timekeeping_processor_face/src/main/java/nhanph/timekeeping/processor/controller/ChatController.java
+++ timekeeping_processor_face/src/main/java/nhanph/timekeeping/processor/controller/ChatController.java
@@ -2,7 +2,6 @@ package nhanph.timekeeping.processor.controller;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 
 @Controller
