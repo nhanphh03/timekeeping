704b05f04fdeb132a0473ef22c8ac5fcb6ea4067
nhanph <nhanphh2003@gmail.com>
2025-05-13 23:07:27 +0700

    change websocket

--- timekeeping_web/src/components/items/day/day-item-list.tsx
+++ timekeeping_web/src/components/items/day/day-item-list.tsx
@@ -9,7 +9,7 @@ export default function DayItemList() {
             <DayItem child = "Wednesday" status = 'inActive' day= '07' month= 'May' sx={{width: '13%', height: 75}}></DayItem>
             <DayItem child = "Thursday" status = 'inActive' day= '08' month= 'May' sx={{width: '13%', height: 75}}></DayItem>
             <DayItem child = "Friday" status = 'inActive' day= '09' month= 'May' sx={{width: '13%', height: 75}}></DayItem>
-            <DayItem child = "Saturday" status = 'active' day= '10' month= 'May'   sx={{width: '13%', height: 75}}></DayItem>
+            <DayItem child = "Saturday" status = 'active' day= '10' month= 'May' sx={{width: '13%', height: 75}}></DayItem>
             <DayItem child = "Sunday" status = 'inActive' day= '11' month= 'May' sx={{width: '13%', height: 75}}></DayItem>
         </Stack>
     )
